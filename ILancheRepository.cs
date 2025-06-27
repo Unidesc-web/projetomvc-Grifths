0ba4a03adabfe22d8d201b80fc1dbaf98810ec8a
Lucas Peixoto <120991634+Grifths@users.noreply.github.com>
2025-06-27 16:26:11 -0300

    Delete ILancheRepository.cs

--- ILancheRepository.cs
+++
@@ -1,9 +0,0 @@
-
-using LanchoneteMVC.Models;
-namespace LanchoneteMVC.Repositories.Interfaces;
-public interface ILancheRepository
-{
-    IEnumerable<Lanche> Lanches { get; }
-    IEnumerable<Lanche> LanchesPreferidos { get; }
-    Lanche GetLancheById(int lancheId);
-}
